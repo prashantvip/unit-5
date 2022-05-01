27d29c4ab330a6a6633d6598bfc2ef7e3fa378e6
Prashant Kumar <kr.prashantjsr@gmail.com>
2022-05-01 11:24:55 +0530

    timer done

---
+++ react-timer/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import './App.css';
+import { Timer } from './conponents/Timer';
+
+function App() {
+  return (
+    <div className="App">
+      <Timer/>
+    </div>
+  );
+}
+
+export default App;
